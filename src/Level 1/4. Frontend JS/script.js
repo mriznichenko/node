0735112b4a4307524c34931b21933ba2924e8232
mriznichenko <riznichenko.mykhailo@gmail.com>
2022-10-07 22:22:09 +0300

    123

--- src/Level 1/4. Frontend JS/script.js
+++ src/Level 1/4. Frontend JS/script.js
@@ -1,2 +1,14 @@
-let a = document.URL;
-console.log(a);
+console.log("script log");
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+
+function foo() {
+    console.log("foo called");
+}
