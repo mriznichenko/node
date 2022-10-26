5014cff082763898e158533685db63cf48a7a57f
mriznichenko <riznichenko.m.d@gmail.com>
2022-10-26 05:16:26 +0300

    123 msg

--- src/Level 1/4. Frontend JS/script.js
+++ src/Level 1/4. Frontend JS/script.js
@@ -1,14 +1,53 @@
-console.log("script log");
+function switchToNextVisibility(e) {
+    return e === "none" ? "inherit" : "none";
+}
 
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
+
+
+
+function changeVisibility(collection) {
+    collection.forEach(e => { e.style.display = switchToNextVisibility(e.style.display) })
+}
+
+
+function practice1_way2() {
+    document.getElementById("frontJSpractice_1").remove();
+}
+
+function practice1_way3() {
+    document.getElementById("frontJSpractice_1").classList.add('hidden');
+}
+
+// 2 
+function practice2() {
+    let classes = document.getElementById("frontJSpractice_1").classList;
+
+    if (classes.contains("hidden")) {
+        classes.remove("hidden")
+    } else {
+        classes.add("hidden")
     }
 }
 
-
-function foo() {
-    console.log("foo called");
+function practice3() {
+    changeVisibility(document.querySelectorAll(".practice3"))
 }
+
+function practice4() {
+    let input = document.getElementById('practice4inputField').value
+    changeVisibility(document.querySelectorAll(input))
+}
+
+function practice5() {
+    let square = document.getElementById("IDpractice5")
+    alert("hello")
+    square.onclick = document.getElementById("IDpractice5").remove
+
+}
+
+function practice6() {
+    let currentVis = document.getElementById("IDpractice6").style.visibility;
+    document.getElementById("IDpractice6").style.visibility = (currentVis === "hidden" ? "visible" : "hidden")
+}
+
+
