8ee37357a8448c7d2c9ca961f17ebcde4d0fc395
mriznichenko <riznichenko.m.d@gmail.com>
2022-12-23 02:23:26 +0200

    1

--- src/lvl2/p2/2.2.4.1.ts
+++ src/lvl2/p2/2.2.4.1.ts
@@ -17,46 +17,15 @@
 
 const https = require('https');
 
-
-let uri = 'random-data-api.com/api/v2/users?size=1&response_type=json'
-
 const baseURI = "random-data-api.com"
 const usersPath = "/api/v2/users"
-const options = {
-    size: 3, // max 100
-    // is_xml: true,
-    // response_type: "json"
-}
 
 type User = {
     gender: string;
 }
 
-function getUsers() {
-    return new Promise((resolve) => {
-        const https = require('https');
-        let data = '';
-        https.request({
-            host: baseURI,
-            path: usersPath + "?size=3&response_type=json",
-        }, (res: any) => {
-
-            res.on('data', (chunk: string) => {
-                data += chunk;
-            });
-
-            res.on('end', () => {
-                console.log(data)
-            });
-        }).end();
-        resolve(data)
-    })
-}
-
-
-
-function getUsersArray() : Promise<User[]> {
-    return new Promise<User[]>((resolve) => {
+function getFemaleUser() : Promise<User> {
+    return new Promise<User>((resolve) => {
         const https = require('https');
         let data = '';
         https.request({
@@ -68,38 +37,12 @@ function getUsersArray() : Promise<User[]> {
             });
 
             res.on('end', () => {
-                resolve(JSON.parse(data))
+                resolve(JSON.parse(data).find( (e : User) => e.gender === "Female"))
             });
         }).end();
     })
 }
 
-// function getFemaleUser() : Promise <User>{
-//     let femalePromise :Promise <User>;
-    
-//     femalePromise = getUsersArray().then(usersArr => {
-//         let fem : = searchFemale(usersArr);
-//         while(fem === undefined || fem?.gender !== "Female") {
-//             fem = getFemaleUser();
-//         }
-//         return fem;
-//     });
-
-//     return femalePromise;
-// }
-
-// function searchFemale(users: User[]) : User | undefined {
-//     let femaleUser;
-//     for (let user of users) {
-//         if (user.gender === "Female") {
-//             femaleUser = user;
-//             break;
-//         }
-//     }
-//     return femaleUser;
-// }
-
-// getFemaleUser().then(e => console.log(e))
-getUsersArray().then(e => console.log(e))
+getFemaleUser().then(e => console.log(e))
 
 export { }
