baa8ef21008074c82c603fd8a12f2bf8ddbf3b25
mriznichenko <riznichenko.m.d@gmail.com>
2023-01-11 23:12:57 +0200

    1

--- src/lvl2/p4/2.4-server.ts
+++ src/lvl2/p4/2.4-server.ts
@@ -1,14 +1,18 @@
-// @ts-nocheck
 import express from "express";
-const SERVER_PORT : number = 12345;
+
 const app = express();
+const SERVER_PORT = 3005;
+
+app.use(express.static(process.cwd() + "/src/lvl2/p4/public"));
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Port ${SERVER_PORT} listening now`)
+    console.log(`Port ${SERVER_PORT} listening now`);
 })
 
 app.get('/', (req, res) => {
-    res.send(`root response, server port = ${SERVER_PORT}`)
+    let time = new Date().toLocaleString("uk-UA", { timeZone: "Europe/Kiev" });
+    res.send(`${time}: hello from port: ${SERVER_PORT}`);
+    console.log(`${time}: response sent`);
 })
 
 export { }
